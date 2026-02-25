7fdadd9ada5acd5524a520c4ee1ac4057e5f7a63
mahadi5272 <mahadihasan5272@gmail.com>
2026-02-25 17:17:06 +0600

    added privateRoute

--- src/Router/PrivateRoute.jsx
+++ src/Router/PrivateRoute.jsx
@@ -1,27 +1,27 @@
-// import { useContext } from "react";
-// import { AuthContext } from "../AuthContext/AuthProvider";
-// import { Navigate, useLocation } from "react-router";
+import { useContext } from "react";
+import { AuthContext } from "../AuthContext/AuthProvider";
+import { Navigate, useLocation } from "react-router";
 
-// const PrivateRoute = ({ children }) => {
-//     const { user, loading } = useContext(AuthContext);
-//     const location = useLocation();
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    const location = useLocation();
 
-//     // যতক্ষণ ডাটা লোড হচ্ছে ততক্ষণ লোডিং স্পিনার দেখাবে
-//     if (loading) {
-//         return (
-//             <div className="flex justify-center items-center min-h-screen">
-//                 <span className="loading loading-spinner loading-lg text-indigo-600"></span>
-//             </div>
-//         );
-//     }
+    // যতক্ষণ ডাটা লোড হচ্ছে ততক্ষণ লোডিং স্পিনার দেখাবে
+    if (loading) {
+        return (
+            <div className="flex justify-center items-center min-h-screen">
+                <span className="loading loading-spinner loading-lg text-indigo-600"></span>
+            </div>
+        );
+    }
 
-//     // ইউজার থাকলে মেইন পেজ দেখাবে
-//     if (user) {
-//         return children;
-//     }
+    // ইউজার থাকলে মেইন পেজ দেখাবে
+    if (user) {
+        return children;
+    }
 
-//     // ইউজার না থাকলে লগইন পেজে পাঠিয়ে দিবে
-//     return <Navigate to="/login" state={{ from: location }} replace />;
-// };
+    // ইউজার না থাকলে লগইন পেজে পাঠিয়ে দিবে
+    return <Navigate to="/login" state={{ from: location }} replace />;
+};
 
-// export default PrivateRoute;
+export default PrivateRoute;
